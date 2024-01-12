8d300517756ad93f7c247af7b862a1eedb57d231
meme <kn.yokota1208@gmail.com>
2024-01-12 13:16:03 +0900

    contactfフォームレイアウト調整

--- contact/index.php
+++ contact/index.php
@@ -25,67 +25,54 @@
 
         <form class="contact__form form" action="" method="post">
           <!-- 体験日程 -->
-          <p class="form__wrap">
-            <label class="form__label">体験日程<span>必須</span>
-              <input type="text" name="date" autocomplete="date" placeholder="日時を選択してください">
+          <div class="form__wrap">
+            <label class="form__label" for="date">体験日程<span>必須</span>
             </label>
-          </p>
+            <div class="form__data">
+              <input type="text" id="date" name="date" autocomplete="date" placeholder="日時を選択してください">
+            </div>
+          </div>
           <!-- お名前 -->
-          <p class="form__wrap">
-            <label class="form__label">お名前<span>必須</span>
-              <input type="text" name="your_name" autocomplete="name">
-            </label>
-          </p>
+          <div class="form__wrap">
+            <label class="form__label" for="name">お名前<span>必須</span></label>
+            <div class="form__data">
+              <input type="text" id="name" name="your_name" autocomplete="name">
+            </div>
+          </div>
           <!-- 性別---ラジオボタン -->
           <div class="form__wrap radio">
-            <p class="form__label">性別</p>
+            <div class="form__label">性別</div>
             <div class="radio__wrap">
               <label><input type="radio" name="gender" value="女性"><span>女性</span></label>
               <label><input type="radio" name="gender" value="男性"><span>男性</span></label>
             </div>
           </div>
           <!-- 住所 -->
-          <p class="form__wrap">
-            <label class="form__label">ご住所<span>必須</span>
-              <input type="text" name="your_address" autocomplete="name">
-            </label>
-          </p>
+          <div class="form__wrap">
+            <label class="form__label" for="address">ご住所<span>必須</span> </label>
+            <div class="form__data">
+              <input type="text" id="address" name="your_address" autocomplete="name">
+            </div>
+          </div>
           <!-- メールアドレス -->
-          <p class="form__wrap">
-            <label class="form__label">メールアドレス<span>必須</span>
-              <input type="email" name="email" autocomplete="email" placeholder="info@example.com">
-            </label>
-          </p>
+          <div class="form__wrap">
+            <label class="form__label" for="mail">メールアドレス<span>必須</span></label>
+            <div class="form__data">
+              <input type="email" id="mail" name="email" autocomplete="email" placeholder="info@example.com">
+            </div>
+          </div>
           <!--  電話番号 -->
-          <p class="form__wrap">
-            <label class="form__label">電話番号<span>必須</span>
-              <input type="tel" name="tel" autocomplete="tel" placeholder="090-1234-5678">
-            </label>
-          </p>
+          <div class="form__wrap">
+            <label class="form__label" for="tel">電話番号<span>必須</span></label>
+            <div class="form__data">
+              <input type="tel" id="tel" name="tel" autocomplete="tel" placeholder="090-1234-5678">
+            </div>
+          </div>
           <!-- 送信ボタン -->
           <div class="form__submit">
             <input type="submit" value="送信する">
           </div>
         </form>
-
-        <!-- テスト -->
-        <div class="inner">
-    <h2 class="title">お問い合わせフォーム<br>No.1 input・textareaをカスタマイズしよう！</h2>
-    <form action="" class="form">
-        <div class="row">
-            <div class="head">氏名</div>
-            <div class="data"><input type="text" placeholder="ヤマダ タロウ"></div>
-        </div>
-        <div class="row">
-            <div class="head">メールアドレス</div>
-            <div class="data"><input type="email" placeholder="info@example.com"></div>
-        </div>
-        <div class="row ai-start">
-            <div class="head">お問い合わせ内容</div>
-            <div class="data"><textarea placeholder="こちらに具体的にご入力ください"></textarea></div>
-        </div>
-    </form>
-</div>
 
       </div>
     </section>
