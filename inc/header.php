d3c97304259b63e4cd860f7f888652e9e1244cb1
meme <kn.yokota1208@gmail.com>
2024-01-21 22:33:15 +0900

    position;relative;→absolute;に変更しました。

--- inc/header.php
+++ inc/header.php
@@ -18,12 +18,6 @@
     <?php endif; ?>
 
 
-  <!-- <h1 class="header__logo">
-        <a href="/">
-          <img src="images/top/logo_header.png" alt="u fit" width="61" height="61">
-          <span class="header__title">U FIT</span>
-        </a>
-      </h1> -->
       <div id="js_hamburgerBg" class="hamburger_bg"></div><!-- is activeが付くとこ -->
       <nav id="js_headerNav" class="header__nav"><!-- is activeが付くとこ -->
         <ul class="header__nav-list">
@@ -34,7 +28,6 @@
           <li class="header__nav-item"><a class="c-btn c-btn--colorChange" href="/contact/">無料体験の申し込みはこちら</a></li>
         </ul>
       </nav>
-      <!-- <a href="" class="c-btn c-btn--white">無料体験お申込み</a> -->
 
       <div class="header__contact-wrapper">
         <a class="header__contact" href="/contact/"></a>
